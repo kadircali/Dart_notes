263dd494b53c24893a07e1b0f237ca78cc2e32f2
kadircali <a.kadircali@hotmail.com>
2024-08-08 22:40:31 +0300

    Dart_notes

---
+++ 001_variables/main.dart
@@ -0,0 +1,90 @@
+void main(List<String> args) {
+  /*
+  built-in types 
+  Numbers(int , double) 
+  Strings(String)
+  booleans(bool)
+
+
+
+  Dart numbers come in two flavors :int and double
+  */
+/*
+  int num1 = 5;
+  double num2 = 1323.3123;
+  print(num1);
+  print(num2);
+
+  var num3 = 10;
+  var num4 = 32.1332;
+  num x = 1; // x can have both int and double values
+
+  //casting
+
+  //num to num
+  int a = 5;
+  double b = a.toDouble();
+  print(b);
+
+  //num to string
+  int c = 123;
+  String text = c.toString();
+
+  //string to num
+  String number = "35";
+  int number2 = int.parse(number);
+  
+
+
+
+  //immutable, Determined at runtime.
+  final int number = 5;
+
+//Determined at compile time
+  const int number2 = 5;
+
+
+
+  //boolean
+bool isValid = true ;
+bool isMarried = false
+
+
+ //String
+var s1 = 'Single quotes work well for string literals.';
+var s2 = "Double quotes work just as well.";
+var s3 = 'It\'s easy to escape the string delimiter.';
+var s4 = "It's even easier to use the other delimiter.";
+
+//dynamic variables
+  var deneme = null;
+  print(deneme);
+
+  dynamic test = "test";
+  print(test);
+
+  //methods
+  String text = "hello world";
+  print("length: ${text.length}");
+
+//index
+  print(text[0]);
+
+  //
+  String newText = text.replaceAll("w", "v");
+  print(newText);
+  print(text.toLowerCase());
+  print(text.toUpperCase());
+
+  //substring
+  print(text.substring(2));
+
+  String text2 = "   test     ";
+  print(text2.trim());
+
+  double number = 99.99;
+  print(number.ceil());
+  print(number.floor());
+  print(number.round());
+*/
+}
